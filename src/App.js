af0a6312c3e6972b0c830e3183cd2e979fdec1ea
MirnesZahirovic <contact@mirneszahirovic.me>
2023-01-15 23:09:02 +0100

    Second chance

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World.</p>
+        <p>Hello World. This is my first React app.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
